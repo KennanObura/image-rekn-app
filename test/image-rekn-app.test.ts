b289f0dcfbd9586a601fc2cb0f59ff54e4a70189
kennanObura <kennan.obura2@gmail.com>
2022-03-10 07:37:00 +0300

    Initial commit

---
+++ test/image-rekn-app.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as ImageReknApp from '../lib/image-rekn-app-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/image-rekn-app-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new ImageReknApp.ImageReknAppStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
