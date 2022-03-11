74633de23d073cdc68db8f949d0e8d2fdb990350
kennanObura <kennan.obura2@gmail.com>
2022-03-11 06:56:56 +0300

    first commit

--- lib/image-rekn-app-stack.ts
+++ lib/image-rekn-app-stack.ts
@@ -1,16 +1,214 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
+import { aws_iam, aws_lambda_event_sources, Duration, Stack, StackProps } from 'aws-cdk-lib';
+import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import s3 = require('aws-cdk-lib/aws-s3');
+import lambda = require('aws-cdk-lib/aws-lambda');
+import dynamoDB = require('aws-cdk-lib/aws-dynamodb');
+import apigw = require('aws-cdk-lib/aws-apigateway');
+import { PassthroughBehavior } from 'aws-cdk-lib/aws-apigateway';
+
+const imageBucketName = 'cdk-rekn-imagebucket';
+const tableName = 'imageLablesTable';
+const resizedBucketName = imageBucketName + "-resized"
 
 export class ImageReknAppStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'ImageReknAppQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const imageBucket = new s3.Bucket(this, imageBucketName);
+    new cdk.CfnOutput(this, 'imageBucket', { value: imageBucket.bucketName });
+
+
+    // =====================================================================================
+    // Thumbnail Bucket
+    // =====================================================================================
+    const resizedBucket = new s3.Bucket(this, resizedBucketName, {
+      removalPolicy: cdk.RemovalPolicy.DESTROY
+    });
+    new cdk.CfnOutput(this, 'resizedBucket', { value: resizedBucket.bucketName });
+
+
+    // =====================================================================================
+    // Amazon DynamoDB table for storing image labels
+    // =====================================================================================
+    const table = new dynamoDB.Table(this, tableName, {
+      partitionKey: { name: 'image', type: dynamoDB.AttributeType.STRING }
+    })
+
+    new cdk.CfnOutput(this, 'table', { value: table.tableName })
+
+
+
+    // =====================================================================================
+    // Building our AWS Lambda Function; compute for our serverless microservice
+    // =====================================================================================
+    const layer = new lambda.LayerVersion(this, 'pil', {
+      code: lambda.Code.fromAsset('reklayer'),
+      compatibleRuntimes: [lambda.Runtime.PYTHON_3_7],
+      license: 'Apache-2.0',
+      description: 'A layer to enable the PIL library in our Rekognition Lambda',
+    });
+
+
+
+
+
+    // =====================================================================================
+    // Building our AWS Lambda Function; compute for our serverless microservice
+    // =====================================================================================
+    const rekFn = new lambda.Function(this, 'rekognitionFunction', {
+      code: lambda.Code.fromAsset('rekognitionlambda'),
+      runtime: lambda.Runtime.PYTHON_3_7,
+      handler: 'index.handler',
+      timeout: Duration.seconds(30),
+      memorySize: 1024,
+      layers: [layer],
+      environment: {
+        "TABLE": table.tableName,
+        "BUCKET": imageBucket.bucketName,
+        "RESIZEDBUCKET": resizedBucket.bucketName
+      },
+    });
+
+
+    rekFn.addEventSource(new aws_lambda_event_sources.S3EventSource(imageBucket, { events: [s3.EventType.OBJECT_CREATED] }));
+    imageBucket.grantRead(rekFn);
+    resizedBucket.grantPut(rekFn);
+    table.grantWriteData(rekFn);
+
+    rekFn.addToRolePolicy(new aws_iam.PolicyStatement({
+      effect: aws_iam.Effect.ALLOW,
+      actions: ['rekognition:DetectLabels'],
+      resources: ['*']
+    }));
+
+
+
+    // =====================================================================================
+    // Lambda for Synchronous Front End
+    // =====================================================================================
+    const serviceFn = new lambda.Function(this, 'serviceFunction', {
+      code: lambda.Code.fromAsset('servicelambda'),
+      runtime: lambda.Runtime.PYTHON_3_7,
+      handler: 'index.handler',
+      environment: {
+        "TABLE": table.tableName,
+        "BUCKET": imageBucket.bucketName,
+        "RESIZEDBUCKET": resizedBucket.bucketName
+      },
+    });
+    imageBucket.grantWrite(serviceFn);
+    resizedBucket.grantWrite(serviceFn);
+    table.grantReadWriteData(serviceFn);
+
+
+
+
+
+    // =====================================================================================
+    // API GATEWAY: REST
+    // =====================================================================================
+
+
+    const api = new apigw.LambdaRestApi(this, 'imageAPI', {
+      defaultCorsPreflightOptions: {
+        allowOrigins: apigw.Cors.ALL_ORIGINS,
+        allowMethods: apigw.Cors.ALL_METHODS
+      },
+      handler: serviceFn,
+      proxy: false
+    })
+
+
+
+
+    // =====================================================================================
+    // This construct builds a new Amazon API Gateway with AWS Lambda Integration
+    // =====================================================================================
+    const lambdaIntegration = new apigw.LambdaIntegration(serviceFn, {
+      proxy: false,
+      requestParameters: {
+        'integration.request.querystring.action': 'method.request.querystring.action',
+        'integration.request.querystring.key': 'method.request.querystring.key'
+      },
+      requestTemplates: {
+        'application/json': JSON.stringify({ action: "$util.escapeJavaScript($input.params('action'))", key: "$util.escapeJavaScript($input.params('key'))" })
+      },
+      passthroughBehavior: PassthroughBehavior.WHEN_NO_TEMPLATES,
+      integrationResponses: [
+        {
+          statusCode: "200",
+          responseParameters: {
+            // We can map response parameters
+            // - Destination parameters (the key) are the response parameters (used in mappings)
+            // - Source parameters (the value) are the integration response parameters or expressions
+            'method.response.header.Access-Control-Allow-Origin': "'*'"
+          }
+        },
+        {
+          // For errors, we check if the error message is not empty, get the error data
+          selectionPattern: "(\n|.)+",
+          statusCode: "500",
+          responseParameters: {
+            'method.response.header.Access-Control-Allow-Origin': "'*'"
+          }
+        }
+      ],
+    });
+
+
+
+
+
+    // =====================================================================================
+    // API Gateway
+    // =====================================================================================
+    const imageAPI = api.root.addResource('images');
+    
+    // GET /images
+    imageAPI.addMethod('GET', lambdaIntegration, {
+      requestParameters: {
+        'method.request.querystring.action': true,
+        'method.request.querystring.key': true
+      },
+      methodResponses: [
+        {
+          statusCode: "200",
+          responseParameters: {
+            'method.response.header.Access-Control-Allow-Origin': true,
+          },
+        },
+        {
+          statusCode: "500",
+          responseParameters: {
+            'method.response.header.Access-Control-Allow-Origin': true,
+          },
+        }
+      ]
+    });
+    
+    // DELETE /images
+    imageAPI.addMethod('DELETE', lambdaIntegration, {
+      requestParameters: {
+        'method.request.querystring.action': true,
+        'method.request.querystring.key': true
+      },
+      methodResponses: [
+        {
+          statusCode: "200",
+          responseParameters: {
+            'method.response.header.Access-Control-Allow-Origin': true,
+          },
+        },
+        {
+          statusCode: "500",
+          responseParameters: {
+            'method.response.header.Access-Control-Allow-Origin': true,
+          },
+        }
+      ]
+    });
+
   }
 }
